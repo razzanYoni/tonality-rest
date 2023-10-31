90993606dee7b3455e679f41cecd1c74f0df6fb2
Noel Simbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-10-31 13:17:25 +0700

    refactor: remove unused code in app.ts

--- src/cores/app.ts
+++ src/cores/app.ts
@@ -3,13 +3,9 @@ import dotenv from "dotenv";
 import apiRouter from "../routers/api";
 
 dotenv.config();
+
 export const app = express();
 const port = process.env.EXPRESS_PORT;
-app.get("/", (req, res) => {
-  res.send({
-    message: "Hello World!",
-  });
-});
 
 app.use(express.json());
 app.use(apiRouter);
