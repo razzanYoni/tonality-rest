4513b32c2b74ac78c930b0859105cbf8bbca613c
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-11-12 15:59:08 +0700

    add cors

--- src/cores/app.ts
+++ src/cores/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import apiRouter from "../routers/api";
 
@@ -9,6 +10,20 @@ export const app: Express = express();
 const port: string | undefined = process.env.EXPRESS_PORT;
 
 app.use(express.json());
+
+const allowedOrigin = ['http://localhost:3000', 'http://localhost:3001', "http://localhost:5173"]
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+    if (allowedOrigin.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+    },
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(apiRouter);
 
